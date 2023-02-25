912a59e089d289aafe6c2a818147847e142c238b
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-02-25 02:29:55 +0100

    feat(event): write `event_params` private method

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,10 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :body, :date)
+  end
 end
