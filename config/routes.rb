6ca8d3ea2179da7a0296fc58319f9b10548db616
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-02-25 02:20:03 +0100

    feat(event): add routes for `events`

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
+  resources :events
   # Defines the root path route ("/")
   # root "articles#index"
   root "events#index"
