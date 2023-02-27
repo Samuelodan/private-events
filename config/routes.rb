5dda15b2ae8110dbf85398acf2a6e81ecc0836b2
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-02-27 22:20:59 +0100

    feat(attendances): add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:show]
   resources :events
+  resources :attendances
   # Defines the root path route ("/")
   # root "articles#index"
   root "events#index"
