70b430924aa2fafa8b5c64a011be803b2ddd6a0b
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-02-28 22:29:20 +0100

    feat: add `dependent: :destroy` to associations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
-  has_many :attendances, foreign_key: "attended_event_id"
-  has_many :attendees, through: :attendances, class_name: "User"
+  has_many :attendances, foreign_key: "attended_event_id", dependent: :destroy
+  has_many :attendees, through: :attendances, class_name: "User", dependent: :destroy
 end
