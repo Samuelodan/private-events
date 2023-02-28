0ae2c31ff98afebd8e634f773b9d8a33643696eb
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-02-28 23:37:41 +0100

    feat(attendance): write `#create` action
    this action should let users become attendees of an event

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,4 +2,13 @@ class AttendancesController < ApplicationController
   def new
     @attendance = Attendance.new
   end
+
+  def create
+    event = Event.find(params[:id])
+    attendance = current_user.attendances.build(attended_event: event)
+
+    if attendance.save
+      redirect_to event, notice: "You've successfully registered for this event."
+    end
+  end
 end
