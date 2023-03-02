e6e606d24d667fd556c6f51244ec2927fc56d8a1
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-02 21:32:28 +0100

    feat(event): write `#update` action

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,6 +25,16 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def update
+    @event = Event.find(params[:id])
+
+    if @event.update(event_params)
+      redirect_to @event
+    else
+      render "edit", status: :unprocessable_entity
+    end
+  end
+
   private
 
   def event_params
