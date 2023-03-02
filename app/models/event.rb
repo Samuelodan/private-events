d0b38fe6905811cd904266a91189fa62a6c9cbf0
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-02 16:13:45 +0100

    feat(event): write `self.past` and `self.upcoming` for model

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :attendances, foreign_key: "attended_event_id", dependent: :destroy
   has_many :attendees, through: :attendances, class_name: "User", dependent: :destroy
+
+  def self.past
+    where("date < ?", DateTime.now)
+  end
+
+  def self.upcoming
+    where("date < ?", DateTime.now)
+  end
 end
