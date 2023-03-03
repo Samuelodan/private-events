824bfb14fa04a6c297c8fae4c92377177e11fdfb
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-03-03 03:22:22 +0100

    feat(attendance): write `#destroy` action

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -11,4 +11,14 @@ class AttendancesController < ApplicationController
       redirect_to event, notice: "You've successfully registered for this event."
     end
   end
+
+  def destroy
+    event = Event.find(params[:event_id])
+    attendance = Attendance.where("attended_event_id = ? AND attendee_id = ? ",
+                                  event.id,
+                                  current_user.id
+                                  ).first
+    attendance.destroy
+    redirect_to event, status: :see_other
+  end
 end
